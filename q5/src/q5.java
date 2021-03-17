24a7511ea17397d8cb27dae5bd635768c8e90bb5
Caio Roberto Souza Santos <caiorss@ucl.br>
2021-03-17 18:51:53 -0300

    att

--- q5/src/q5.java
+++ q5/src/q5.java
@@ -1,13 +1,8 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.io.Writer;
-
 import javax.swing.JOptionPane;
 
 public class q5 {
@@ -27,12 +22,14 @@ public class q5 {
             PrintWriter arq = new PrintWriter(fw);
             textoSalvar = JOptionPane.showInputDialog("Escreva algo para salvar no arquivo: ");
             arq.write(textoSalvar);
+            arq.close();
 
         }catch(FileNotFoundException e){
             System.out.println(nomeArq+" nao existe, será criado um novo");
             textoSalvar = JOptionPane.showInputDialog("Escreva algo para salvar no arquivo: ");
             BufferedWriter arq = new BufferedWriter(new FileWriter(nomeArq));
             arq.write( textoSalvar );
+            arq.close();
         }
       
 
