e90915647de827f2edb6205d6f88165cbc4a31af
Caio Roberto Souza Santos <caiorss@ucl.br>
2021-03-18 19:31:53 -0300

    caio

--- q7/src/q7.java
+++ q7/src/q7.java
@@ -4,6 +4,7 @@ public class q7 {
     public static void main(String[] args) throws Exception {
         String n1, n2, op;
         int num1,num2;
+        char op1;
 
         n1 = JOptionPane.showInputDialog("Numero 1: ");
 
@@ -11,27 +12,31 @@ public class q7 {
 
         
         op = JOptionPane.showInputDialog("Digite a operação (+,-,*,/): ");
+        op1 = op.charAt(0);
 
+        System.out.println(op1);
 
         n2= JOptionPane.showInputDialog("Numero 2: ");
 
         num2 = Integer.parseInt(n2); 
 
-        while(op=="/" && num2==0){
+        while(op1=='/' && num2==0){
             n2= JOptionPane.showInputDialog("Não é permitido divisão por 0, selecione o numero 2 novamente: ");
             num2 = Integer.parseInt(n2);
         }
 
-        if(op=="+"){
+
+        if(op1=='+'){
+            System.out.println(num1+num2);
             JOptionPane.showMessageDialog(null, "Soma = " + (num1+num2), "Resultado", JOptionPane.PLAIN_MESSAGE );
         }
-        if(op=="-"){
+        if(op1=='-'){
             JOptionPane.showMessageDialog(null, "Subtração = " + (num1-num2), "Resultado", JOptionPane.PLAIN_MESSAGE );
         } 
-        if(op=="*"){
+        if(op1=='*'){
             JOptionPane.showMessageDialog(null, "Multiplicação = " + (num1*num2), "Resultado", JOptionPane.PLAIN_MESSAGE );
         } 
-        if(op=="/"){
+        if(op1=='/'){
             JOptionPane.showMessageDialog(null, "Divisão = " + (num1/num2), "Resultado", JOptionPane.PLAIN_MESSAGE );
         } 
 
